0e1544dd0f90593afcb80b30f58a869953f806ee
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-10-17 07:25:56 +0300

    домашняя работа №13

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         Author author2 = new Author("Chris", "Frith");
 
         Book lotr = new Book("The Lord of the Rings ", author1, 1953);
-        System.out.println(lotr.toString());
+        System.out.println(lotr);
         lotr.setYear(1954);
-        System.out.println(lotr.toString());
+        System.out.println(lotr);
 
         Book brain = new Book("Making up the mind ", author2, 2007);
         System.out.println(brain.toString());
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(bookTest1.toString());
         Author authorTest2 = new Author("Suzanne", "Collins");
         Book bookTest2 = new Book("Gregor the Overlander", authorTest2, 2003);
-        System.out.println(bookTest2.toString());
+        System.out.println(bookTest2);
         System.out.println(authorTest1.equals(authorTest2));
         System.out.println(bookTest1.equals(bookTest2));
     }
